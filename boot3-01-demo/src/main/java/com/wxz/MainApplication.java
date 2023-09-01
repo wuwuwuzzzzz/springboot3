f1631b480facf9d1bb7b538728f5164f4ffc6f48
wxz <759126349@qq.com>
2023-09-01 09:33:35 +0800

    优化代码

--- boot3-01-demo/src/main/java/com/wxz/MainApplication.java
+++ boot3-01-demo/src/main/java/com/wxz/MainApplication.java
@@ -1,7 +1,15 @@
 package com.wxz;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wxz
+ * @date 21:13 2023/8/31
+ */
+@SpringBootApplication
+public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(MainApplication.class, args);
     }
 }
--- boot3-01-demo/src/main/java/com/wxz/controller/HelloController.java
+++ boot3-01-demo/src/main/java/com/wxz/controller/HelloController.java
@@ -1,2 +1,22 @@
-package com.wxz.controller;public class HelloController {
+package com.wxz.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wxz
+ * @date 21:14 2023/8/31
+ */
+@RestController
+public class HelloController {
+
+    /**
+     * @return java.lang.String
+     * @author wxz
+     * @date 21:15 2023/8/31
+     */
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot 3!";
+    }
 }
